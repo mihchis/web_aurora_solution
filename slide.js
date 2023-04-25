7fbc6f1d1643e2c5df8a557331b3ed4ab74aeeda
mihchis <trin79136@gmail.com>
2023-04-25 09:25:13 +0700

    update giai phap

--- slide.js
+++ slide.js
@@ -5,7 +5,7 @@ $('.list-item-slide').slick({
   arrows: false,
   dots: true,
   autoplay: true,  // Thêm tùy chọn autoplay
-  autoplaySpeed: 2000,  // Tự động trượt sau mỗi 2 giây
+  autoplaySpeed: 700,  // Tự động trượt sau mỗi 2 giây
   prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-arrow-right'></i></button>",
   nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa-solid fa-arrow-left'></i></button>",
 });
