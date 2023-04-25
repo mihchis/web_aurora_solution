5f0ef657f477cc7f85833a52c4ae14c1d2095077
mihchis <trin79136@gmail.com>
2023-04-25 09:25:40 +0700

    update giai phap

--- header.js
+++ header.js
@@ -1,16 +1,16 @@
-window.onload = function() {
-  var gioiThieu = document.getElementById('gioithieu');
-  var dichvu = document.getElementById('dichvu');
-  var sanpham = document.getElementById('sanpham');
-  var lienhe = document.getElementById('lienhe');
+window.onload = function () {
+  var gioiThieu = document.getElementById("gioithieu");
+  var dichvu = document.getElementById("dichvu");
+  var sanpham = document.getElementById("sanpham");
+  var lienhe = document.getElementById("lienhe");
 
-  if (window.location.hash === '#gioithieu' && gioiThieu) {
+  if (window.location.hash === "#gioithieu" && gioiThieu) {
     window.scrollTo(0, gioiThieu.offsetTop);
-  } else if (window.location.hash === '#dichvu' && dichvu) {
+  } else if (window.location.hash === "#dichvu" && dichvu) {
     window.scrollTo(0, dichvu.offsetTop);
-  } else if (window.location.hash === '#sanpham' && sanpham) {
+  } else if (window.location.hash === "#sanpham" && sanpham) {
     window.scrollTo(0, sanpham.offsetTop);
-  } else if (window.location.hash === '#lienhe' && lienhe) {
+  } else if (window.location.hash === "#lienhe" && lienhe) {
     window.scrollTo(0, lienhe.offsetTop);
   }
 };
--- slide.js
+++ slide.js
@@ -1,11 +1,13 @@
-$('.list-item-slide').slick({
-  infinite: true,
+$(".list-item-slide").slick({
+  // infinite: true,
   slidesToShow: 1,
   slidesToScroll: 1,
   arrows: false,
   dots: true,
-  autoplay: true,  // Thêm tùy chọn autoplay
-  autoplaySpeed: 700,  // Tự động trượt sau mỗi 2 giây
-  prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-arrow-right'></i></button>",
-  nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa-solid fa-arrow-left'></i></button>",
+  autoplay: true,
+  autoplaySpeed: 2500,
+  prevArrow:
+    "<button type='button' class='slick-prev pull-left'><i class='fa-solid fa-arrow-right'></i></button>",
+  nextArrow:
+    "<button type='button' class='slick-next pull-right'><i class='fa-solid fa-arrow-left'></i></button>",
 });
